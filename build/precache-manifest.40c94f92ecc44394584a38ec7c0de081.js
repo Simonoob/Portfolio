32566e2873656568677085c25205560e3fb5e1e9
Simone Fiorentini <simonefiorentini1@gmail.com>
2020-09-14 11:09:21 +0300

    added build component (deployment ready)

---
+++ build/precache-manifest.40c94f92ecc44394584a38ec7c0de081.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7b49641faf1ad3dd89e2bdec641350a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6ec1be6e3bc3540f68d",
+    "url": "/static/css/main.ea58a23e.chunk.css"
+  },
+  {
+    "revision": "0b0e7e2f2281f195013a",
+    "url": "/static/js/2.c03a51be.chunk.js"
+  },
+  {
+    "revision": "c718c70012c37585b769d1f93ca6c7f1",
+    "url": "/static/js/2.c03a51be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6ec1be6e3bc3540f68d",
+    "url": "/static/js/main.f38cded5.chunk.js"
+  },
+  {
+    "revision": "f5a103a642fb380309d5",
+    "url": "/static/js/runtime-main.23cd9947.js"
+  },
+  {
+    "revision": "4cd3b2b446ec7fe241401deca3057332",
+    "url": "/static/media/arrow-left.4cd3b2b4.svg"
+  },
+  {
+    "revision": "116a70ada1a49d26fac6b51742f49e34",
+    "url": "/static/media/arrow-right.116a70ad.svg"
+  },
+  {
+    "revision": "2a5b7c1028d6deb7e3fd161a071afa31",
+    "url": "/static/media/book-library.2a5b7c10.PNG"
+  },
+  {
+    "revision": "0f3ab4d8f260e2e343b63b98c1a6502a",
+    "url": "/static/media/job.finder.0f3ab4d8.PNG"
+  },
+  {
+    "revision": "5c6a75f2143f3fde1e825523dff65d21",
+    "url": "/static/media/phonebook.5c6a75f2.PNG"
+  }
+]);
